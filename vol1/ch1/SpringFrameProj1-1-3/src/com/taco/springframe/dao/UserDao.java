983c77b600f910a7952c59a47ee18bb1a6f99d2c
takhoyeong <xkrghdud123@naver.com>
2023-07-03 10:35:05 +0900

    package renamed

--- vol1/ch1/SpringFrameProj1-1-3/src/com/taco/springframe/dao/UserDao.java
+++ vol1/ch1/SpringFrameProj1-1-3/src/com/taco/springframe/dao/UserDao.java
@@ -1,4 +1,4 @@
-package com.intheeast.springframe.dao;
+package com.taco.springframe.dao;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -6,7 +6,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-import com.intheeast.springframe.domain.User;
+import com.taco.springframe.domain.User;
 
 
 public class UserDao {
--- vol1/ch1/SpringFrameProj1-1-3/src/com/taco/springframe/domain/User.java
+++ vol1/ch1/SpringFrameProj1-1-3/src/com/taco/springframe/domain/User.java
@@ -1,4 +1,4 @@
-package com.intheeast.springframe.domain;
+package com.taco.springframe.domain;
 
 public class User {
 	String id;
