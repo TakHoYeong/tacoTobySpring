efeeedf8e31005cbf12ad00b4abf2553d9cdf97c
takhoyeong <xkrghdud123@naver.com>
2023-09-21 23:55:17 +0900

    20230921

--- vol1/ch6/SpringFrameProj6-3-4/src/com/intheeast/springframe/service/UserService.java
+++ vol1/ch6/SpringFrameProj6-3-4/src/com/intheeast/springframe/service/UserService.java
@@ -1,9 +1,21 @@
 package com.intheeast.springframe.service;
 
 import com.intheeast.springframe.domain.User;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
+@Transactional
 public interface UserService {
 	void add(User user);
+	void deleteAll();
+	void update(User user);
 	void upgradeLevels();
+
+	@Transactional
+	User get(String id);
+
+	@Transactional
+	List<User> getAll();
 }
 
